ff151d5300349a0718d736f62a3b3255da36ddb4
LilSix <user36@admin36.local>
2017-05-11 18:54:18 +0800

    Init project.

---
+++ Project_BTM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project_BTM
+//
+//  Created by user36 on 2017/5/11.
+//  Copyright © 2017年 user36. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
