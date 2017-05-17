5f0c62f045878665d46fd9a0d9001e391a6e3373
Lil Six <geminijune08@gmail.com>
2017-05-18 07:58:31 +0800

    Change JSON url.

--- Project_BTM/SearchViewController.m
+++ Project_BTM/SearchViewController.m
@@ -8,20 +8,63 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController ()
+@interface SearchViewController ()<UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating> {
+
+    NSArray *jsonArray;
+    UISearchController *mySearchController;
+    
+}
+
+
 
 @end
 
 @implementation SearchViewController
 
+#pragma mark - View Methods
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // /v2/Bus/Route/City/{City}/{RouteName}    取得指定[縣市],[路線名稱]的路線資料
+    NSURL *url = [NSURL URLWithString:@"http://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taipei/14?$format=JSON"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    NSError *error;
+    jsonArray = [NSJSONSerialization JSONObjectWithData:data
+                                                options:NSJSONReadingMutableContainers
+                                                  error:&error];
+    for (NSDictionary *jsonDictionary in jsonArray) {
+        NSDictionary *routeName = [jsonDictionary objectForKey:@"RouteName"];
+        NSDictionary *zhTW = [routeName objectForKey:@"Zh_tw"];
+        NSLog(@"Zh_tw: %@", zhTW);
+    }
+    
+    [mySearchController setSearchResultsUpdater:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDataSource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [jsonArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *tableViewCell = [tableView dequeueReusableCellWithIdentifier:@"Basic Cell"
+                                                                 forIndexPath:indexPath];
+    [[tableViewCell textLabel] setText:@"Title"];
+    
+    return tableViewCell;
+}
+
+#pragma mark - UISearchResultsUpdating Method
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
 }
 
 /*
