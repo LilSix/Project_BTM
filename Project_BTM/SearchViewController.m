e0eef03c414f74e266af9c5e514015a5e30f32ed
LilSix <user36@admin36.local>
2017-05-23 21:17:31 +0800

    Design search bar and create picker view.

--- Project_BTM/SearchViewController.m
+++ Project_BTM/SearchViewController.m
@@ -6,20 +6,26 @@
 #import "SearchViewController.h"
 #import "CityBus.h"
 
-@interface SearchViewController ()<UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate, UISearchDisplayDelegate> {
+
+@interface SearchViewController ()<UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource> {
+    
     NSMutableArray *cityBusList;
     NSMutableArray *departureStopName;
     NSMutableArray *destinationStopName;
     NSArray *searchResults;
     NSString *stringURL;
     UISearchController *searchController;
+    NSArray *arrayRouteName;
 }
 
 @property (weak, nonatomic) IBOutlet UITableView *searchResultsList;
-//@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
-@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
-@property (strong, nonatomic) IBOutlet UISearchController *searchDisplayController;
+//@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+//@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+//@property (strong, nonatomic) IBOutlet UISearchController *searchDisplayController;
+
+@property (weak, nonatomic) IBOutlet UITextField *routeNameTextField;
+@property (weak, nonatomic) IBOutlet UIPickerView *routeNamePicker;
 
 
 @end
@@ -33,9 +39,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [_searchResultsList setDataSource:self];
-    [_searchResultsList setDelegate:self];
-    [searchController setSearchResultsUpdater:self];
+//    [_searchResultsList setDataSource:self];
+//    [_searchResultsList setDelegate:self];
+//    [searchController setSearchResultsUpdater:self];
+    [_routeNameTextField setDelegate:self];
+    [_routeNamePicker setDelegate:self];
+    [_routeNamePicker setDataSource:self];
+    [_routeNamePicker setHidden:YES];
+    
+    arrayRouteName =@[@"1", @"2", @"3", @"4", @"5", @"6"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -53,6 +66,7 @@
         cityBusList = [NSMutableArray array];
         departureStopName = [NSMutableArray array];
         destinationStopName = [NSMutableArray array];
+//        arrayRouteName = [NSMutableArray array];
 
         
         // http://ptx.transportdata.tw/MOTC/Swagger/#!/CityBusApi/CityBusApi_Route_0
@@ -60,8 +74,10 @@
 //        NSURL *url = [NSURL URLWithString:@"http://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taipei/232?$format=JSON"];
         NSURL *taipeiURL = [NSURL URLWithString:@"http://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taipei?$format=JSON"];
         NSURL *newTaipeiURL = [NSURL URLWithString:@"http://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/NewTaipei?$format=JSON"];
+        
         NSData *taipeiData = [NSData dataWithContentsOfURL:taipeiURL];
         NSData *newTaipeiData = [NSData dataWithContentsOfURL:newTaipeiURL];
+        
         NSError *error;
         NSArray *taipeiCityBus = [NSJSONSerialization JSONObjectWithData:taipeiData
                                                              options:NSJSONReadingMutableContainers
@@ -118,7 +134,9 @@
         [[tableViewCell textLabel] setText:[cityBusList objectAtIndex:[indexPath row]]];
         NSString *stringDepartureStopName = [departureStopName objectAtIndex:[indexPath row]];
         NSString *stringDestinationStopName = [destinationStopName objectAtIndex:[indexPath row]];
-        NSString *stringDetailTextLabel = [NSString stringWithFormat:@"%@－%@", stringDepartureStopName, stringDestinationStopName];
+        NSString *stringDetailTextLabel = [NSString stringWithFormat:@"%@－%@",
+                                           stringDepartureStopName,
+                                           stringDestinationStopName];
         [[tableViewCell detailTextLabel] setText:stringDetailTextLabel];
     } else {
         [[tableViewCell textLabel] setText:[searchResults objectAtIndex:[indexPath row]]];
@@ -129,40 +147,76 @@
 }
 
 
-#pragma mark - UISearchResultsUpdating
+#pragma mark - UIPickerViewDataSource
 
-- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
-    if ([searchController isActive]) {
-        NSString *string = [[searchController searchBar] text];
-        NSLog(@"string: %@", string);
-        if ([string length] > 0) {
-            NSPredicate *predicate = [NSPredicate predicateWithFormat:string];
-            searchResults = [cityBusList filteredArrayUsingPredicate:predicate];
-        } else {
-            searchResults = nil;
-        }
-    } else {
-        searchResults = nil;
-    }
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     
-    [_searchResultsList reloadData];
+    return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    
+    return [arrayRouteName count];
 }
 
 
-#pragma mark - UISearchBarDelegate
+#pragma mark - UIPickerViewDelegate
 
-- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
-    NSString *string = [searchBar text];
-    NSLog(@"string: %@", string);
-    if ([string length] > 0) {
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:string];
-        searchResults = [cityBusList filteredArrayUsingPredicate:predicate];
-    } else {
-        searchResults = nil;
-    }
-
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+    return [arrayRouteName objectAtIndex:row];
 }
 
+
+//#pragma mark - UISearchResultsUpdating
+//
+//- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+//    if ([searchController isActive]) {
+//        NSString *string = [[searchController searchBar] text];
+//        NSLog(@"string: %@", string);
+//        if ([string length] > 0) {
+//            NSPredicate *predicate = [NSPredicate predicateWithFormat:string];
+//            searchResults = [cityBusList filteredArrayUsingPredicate:predicate];
+//        } else {
+//            searchResults = nil;
+//        }
+//    } else {
+//        searchResults = nil;
+//    }
+//    
+//    [_searchResultsList reloadData];
+//}
+
+
+//#pragma mark - UISearchBarDelegate
+//
+//- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+//    NSString *string = [searchBar text];
+//    NSLog(@"string: %@", string);
+//    if ([string length] > 0) {
+//        NSPredicate *predicate = [NSPredicate predicateWithFormat:string];
+//        searchResults = [cityBusList filteredArrayUsingPredicate:predicate];
+//    } else {
+//        searchResults = nil;
+//    }
+//
+//}
+
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    NSLog(@"%@", arrayRouteName);
+    
+    [_routeNamePicker setDelegate:self];
+    [_routeNamePicker setDataSource:self];
+    [_routeNamePicker setHidden:NO];
+    
+    
+    return false;
+}
 
 /*
 #pragma mark - Navigation
